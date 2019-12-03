b8bf50bd1dedaa2d29c1f1736dfdbfed8b5b739c
서영래 <yr1633@gmail.com>
2019-12-03 09:56:34 +0900

    191203 commit 3
     1. branch 생성(other_branch2)
     2. main 패키지 구성

---
+++ app/src/main/java/com/tumedia/mediaandroidpractice/main/MainContract.java
@@ -0,0 +1,13 @@
+package com.tumedia.mediaandroidpractice.main;
+
+import com.tumedia.mediaandroidpractice.base.BasePresenter;
+import com.tumedia.mediaandroidpractice.base.BaseView;
+
+public class MainContract {
+    public interface View extends BaseView {
+    }
+
+    public interface Presenter extends BasePresenter<View> {
+
+    }
+}
