e15bef9efbe92ac17e4777f2fd4a8d32ff5866d5
서영래 <yr1633@gmail.com>
2020-03-15 23:36:26 +0900

    2020-03-15 final commit
    1. DataSource Impl 클래스 구분하여 생성
     1) LocalDataSourceImpl: 로컬에서 데이터 받아오는 datasource 구현 클래스
     2) RemoteDataSourceImpl: 서버에서 데이터 받아오는 datasource 구현 클래스

---
+++ app/src/main/java/com/tumedia/mediaandroidpractice/logic/local/LocalDataSourceImpl.java
@@ -0,0 +1,11 @@
+package com.tumedia.mediaandroidpractice.logic.local;
+
+/**
+ * 로컬(DB)에서 데이터 받아오는 클래스
+ *
+ * @author 서영래(XIMON)
+ * @version 1.0.0
+ * @since 2020-03-15 오후 11:28
+ **/
+public class LocalDataSourceImpl {
+}
--- app/src/main/java/com/tumedia/mediaandroidpractice/logic/remote/RemoteDataSourceImpl.java
+++ app/src/main/java/com/tumedia/mediaandroidpractice/logic/remote/RemoteDataSourceImpl.java
@@ -1,4 +1,11 @@
 package com.tumedia.mediaandroidpractice.logic.remote;
 
+/**
+ * 서버(원격)에서 데이터 받아오는 클래스
+ *
+ * @author 서영래(XIMON)
+ * @version 1.0.0
+ * @since 2020-03-15 오후 11:28
+ **/
 public class RemoteDataSourceImpl {
 }
