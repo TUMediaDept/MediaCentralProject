2cf4d0e3fa4678422bf00088fe248ed198636042
yr163 <yr1633@gmail.com>
2020-03-15 20:05:57 +0900

    2020-03-15
    1. 권한 설정 화면 추가

--- app/src/main/java/com/tumedia/mediaandroidpractice/intro/IntroActivity.java
+++ app/src/main/java/com/tumedia/mediaandroidpractice/intro/IntroActivity.java
@@ -1,10 +1,12 @@
 package com.tumedia.mediaandroidpractice.intro;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import com.tumedia.mediaandroidpractice.R;
 import com.tumedia.mediaandroidpractice.base.BaseActivity;
-import com.tumedia.mediaandroidpractice.main.MainContract;
 
 /**
  * Created by XIMON on 2020-02-05
@@ -22,9 +24,26 @@ public class IntroActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        checkPermission();
     }
 
-    private void checkPermission(){
-
+    // 다른 앱 위에 표시 (오버레이) 권한 체크
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우 체크
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(this,PermissionActivity.class);
+                startActivity(intent);
+                finish();
+//                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            } else {
+//                Log.e("myLog", "startService")
+//                startService()
+            }
+        } else {
+//            startService()
+        }
     }
+
+
 }
